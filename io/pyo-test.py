38df18d423183b9495fa1bc1b579447f2cb77cf0
ConnorWhalen <caw742@mun.ca>
2024-05-18 16:28:18 -0230

    map frequency bands to rgb. Add slider to transform colours

--- io/pyo-test.py
+++ io/pyo-test.py
@@ -1,3 +1,4 @@
+import colorsys
 from pyo import *
 
 s = Server(nchnls=1).boot()
@@ -19,17 +20,37 @@ sel = Selector([tone, butlp, mooglp])
 sel.ctrl(title="Filter selector (0=Tone, 1=ButLP, 2=MoogLP)")
 
 # Splits signal into frequency bands
-mb = MultiBand(sel).out()
+mb = MultiBand(sel, num=3).out()
 
 # Displays the spectrum contents of the chosen source
 sp = Spectrum(mb)
 
+cs = Selector([Noise(0.5), Noise(0.5)])
+cs.ctrl(title="Colour selector")
 
-def print_rms(*amplitudes):
-    print(amplitudes)
+
+def rms_to_rgb(*amplitudes):
+    angle = cs.voice.get() * 2.0 * math.pi
+
+    r_in = amplitudes[0]
+    g_in = amplitudes[1]
+    b_in = amplitudes[2]
+
+    h, s, v = colorsys.rgb_to_hsv(r_in, g_in, b_in)
+
+    h += angle
+    if h > 2.0 * math.pi:
+        h -= 2.0 * math.pi
+
+    r, g, b = colorsys.hsv_to_rgb(h, s, v)
+
+    print(f"IN:  {r_in} {g_in} {b_in}")
+    print(f"OUT: {r} {g} {b}")
 
 # Reports RMS of each band and passes them into callback function
 rms = RMS(mb, function=print_rms)
 rms.polltime(1)
 
 s.gui(locals())
+
+
