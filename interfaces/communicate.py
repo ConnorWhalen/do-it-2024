fbdc79e82b21f45ee87e2c5bd9eea4e9d2651239
gerard <gnoseworthy@gmail.com>
2024-05-18 20:23:35 -0230

    feat: serial interfacing

--- interfaces/communicate.py
+++ interfaces/communicate.py
@@ -1,14 +1,30 @@
 import serial
 import time
-
+import random
+COMMA = ','
 # open a serial connection
 s = serial.Serial("/dev/ttyACM0", 115200)
 
+def rand_8bit():
+    return random.randint(0, 255)
+
+def rand_blue():
+    return random.randint(0, 128)
+
+def send_integers_as_string(ser, int1, int2, int3):
+    data = f"{int1},{int2},{int3}\n"
+    ser.write(data.encode())
+
 # blink the led
 while True:
-    s.write(b"on\n")
-    time.sleep(1)
-    s.write(b"188,102,64\n")
-    time.sleep(1)
-    s.write(b"off\n")
-    time.sleep(1)
+
+    send_integers_as_string(s, rand_8bit(), rand_blue(), rand_blue())
+    time.sleep(0.5)
+    s.write(b"255,5,4\n")
+    time.sleep(0.5)
+    # time.sleep(1)
+    # s.write(b"0,0,254\n")
+    # time.sleep(1)
+    # s.write(b"254,0,0\n")
+    # time.sleep(1)
+
--- interfaces/controller_micro.py
+++ interfaces/controller_micro.py
@@ -1,4 +1,5 @@
 from machine import Pin, PWM
+import sys
 from time import sleep
 
 DUTY_MAX = 65025
@@ -9,6 +10,19 @@ pwm_r = PWM(Pin(11))
 pwm_b.freq(50)
 pwm_g.freq(50)
 pwm_r.freq(50)
+
+while True:
+    # read a command from the host
+    v = sys.stdin.readline().strip()
+
+    # perform the requested action
+
+
+    r_value, g_value, b_value = [i for i in v.split(',')]
+    pwm_b.duty_u16(b_value * 255)
+    pwm_g.duty_u16(g_value * 255)
+    pwm_r.duty_u16(r_value * 255)
+
 
 print('do the thing')
 while True:
